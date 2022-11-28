dbcb64269bf9b74807b0acee7f64feb683e38cc6
scanAskSolve <b10430230@outlook.com>
2022-11-28 23:00:49 +0800

    first commit

---
+++ src/main/java/com/yijyun/phantommask/PhantomMaskApplication.java
@@ -0,0 +1,13 @@
+package com.yijyun.phantommask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantomMaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhantomMaskApplication.class, args);
+    }
+
+}
